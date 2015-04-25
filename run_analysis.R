cc35e0e2dcb2505cb2ac0c68a0dbf8a9c5675cfc
fransch <fschiaffino@gmail.com>
2015-04-25 10:59:26 -0500

    Task 1: run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -20,3 +20,4 @@ setwd("~/Desktop/Coursera R Programming/UCI HAR Dataset")
 > all_data <- cbind(x_data, y_data, subject_data)
 > averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
 > write.table(averages_data, "averages_data2.txt", row.name=FALSE)
+
